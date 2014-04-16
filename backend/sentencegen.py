d74635f01ec39cd324453a587e5d479a24771a83
Kafan Tsumare <juliet@pounces.me>
2014-04-16 23:53:26 +0000

    Add sentence generator

---
+++ backend/sentencegen.py
@@ -0,0 +1,36 @@
+import MySQLdb
+import MySQLdb.cursors
+import random
+
+import credentials
+
+conn = MySQLdb.connect(
+		user=credentials.db_user,
+		passwd=credentials.db_pass,
+		db=credentials.db_name,
+		cursorclass=MySQLdb.cursors.DictCursor,
+		use_unicode=True
+		)
+
+def sentencegen(key):
+	if key[0] != '<' or key[-1] != '>':
+		return key
+	key = key[1:-1]
+
+	with conn as cur:
+		cur.execute('SELECT fragment, weight FROM sentencegen WHERE code = %s', (key,))
+		fragment = None
+		total = 0
+		for row in cur:
+			total += row['weight']
+			if random.randint(0, total) <= row['weight']:
+				fragment = row['fragment']
+
+		if fragment is None: # no db matches
+			return key # handle primitives we havent defined yet
+
+		sentence = map(lambda x: sentencegen(x) if (len(x)>2 and x[0]=='<' and x[-1]=='>') else x, fragment.split(' '))
+		return ' '.join(sentence)
+
+print 'Try:'
+print '>>> sentencegen(\'<sentence_a1>\')'

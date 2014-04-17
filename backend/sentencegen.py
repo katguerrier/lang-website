de3d30a5e4139d754d65000148b2150173907c70
Kafan Tsumare <juliet@pounces.me>
2014-04-17 00:23:54 +0000

    -1 todolist item

--- backend/sentencegen.py
+++ backend/sentencegen.py
@@ -27,14 +27,13 @@ def sentencegen(key):
 				fragment = row['fragment']
 
 		if fragment is None: # no db matches
-			return key # handle primitives we havent defined yet
+			return '<'+key+'>' # handle primitives we havent defined yet
 
 		sentence = map(lambda x: sentencegen(x) if (len(x)>2 and x[0]=='<' and x[-1]=='>') else x, fragment.split(' '))
 		return ' '.join(sentence)
 
 # TODO list off the top of my head:
 # - proper split algorithm
-# - dont take off <>
 # - support alternate db lookups
 # - factor skillmin
 # - factor skillval, and weight adjustment algorithm
